db95cd6db3313c51d22dd89d326567978e0f2e97
Călin Sălăgean <salagean.calin@gmail.com>
2013-07-29 12:23:12 +0300

    add uploadImage operation

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// The code for client side goes here
+module.exports = function (config) {
+
+    var self = this;
+    var options = {};
+
+    $('button#uploadFileBtn').click(function(){
+        options.separator = $('#separator option:selected').text();
+        options.charset = $('#charset option:selected').text();
+        options.headers = $('.checkbox').is(':checked');
+    });
+
+}
--- operations.js
+++ operations.js
@@ -5,6 +5,12 @@ exports.import = function (link) {
     }
 }
 
+exports.uploadImage = function (link) {
+    if (!link.files || !link.files.image || !link.files.image.size) {
+        return link.send(400, { error: 'Invalid image upload' });
+    }
+}
+
 exports.export = function (link) {
     if (!link.data) {
         link.send(200, 'No data sent to me');
