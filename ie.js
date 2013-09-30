17564e2ff7f680b3a5e66bd1dc94ab94351e7dcc
Ionica Bizau <bizauionica@yahoo.com>
2013-09-30 17:04:03 +0300

    Commented unused functions (they were after return; )

--- ie.js
+++ ie.js
@@ -53,49 +53,48 @@ module.exports = function (config) {
         });
     }
 
-    return;
 
     self.on('reset', reset);
 
-    self.emit('getTemplates', function(err, data) {
+    // self.emit('getTemplates', function(err, data) {
 
-        // TODO show an error
-        if (err) { return; }
+    //     // TODO show an error
+    //     if (err) { return; }
 
-        // TODO stop the waiter
-        self.templates = data;
-        console.log('data')
-        setOptionsToSelect(document.getElementById('inputTemplate'), self.templates, { value: 'id', name: 'label' });
-    });
+    //     // TODO stop the waiter
+    //     self.templates = data;
+    //     console.log('data')
+    //     setOptionsToSelect(document.getElementById('inputTemplate'), self.templates, { value: 'id', name: 'label' });
+    // });
 
     // Change the state of upload button
-    $('#csvUpload').change(function () {
-        var fileName = $(this).val().split('.');
-        var extension = fileName[fileName.length-1].toLowerCase();
+    // $('#csvUpload').change(function () {
+    //     var fileName = $(this).val().split('.');
+    //     var extension = fileName[fileName.length-1].toLowerCase();
 
-        if (extension != 'csv') {
-            $('#importStage2Btn').attr('disabled', 'disabled');
-        } else {
-            $('#importStage2Btn').removeAttr('disabled');
-        }
-    });
+    //     if (extension != 'csv') {
+    //         $('#importStage2Btn').attr('disabled', 'disabled');
+    //     } else {
+    //         $('#importStage2Btn').removeAttr('disabled');
+    //     }
+    // });
 
     // set options to a given select tag
-    function setOptionsToSelect (selectElem, data, keys) {
-        var $options = $("<div>");
-        for (var key in data) {
-            // the option name might be with i18n
-            var name = '';
-            if (typeof data[key][keys.name] === 'object') {
-                name = data[key][keys.name][M.getLocale()];
-            } else {
-                name = data[key][keys.name];
-            }
-            var $option = $("<option>").attr("value", data[key][keys.value]).text(name);
-            $options.append($option);;
-        }
-        $(selectElem).html($options.html());
-    };
+    // function setOptionsToSelect (selectElem, data, keys) {
+    //     var $options = $("<div>");
+    //     for (var key in data) {
+    //         // the option name might be with i18n
+    //         var name = '';
+    //         if (typeof data[key][keys.name] === 'object') {
+    //             name = data[key][keys.name][M.getLocale()];
+    //         } else {
+    //             name = data[key][keys.name];
+    //         }
+    //         var $option = $("<option>").attr("value", data[key][keys.value]).text(name);
+    //         $options.append($option);;
+    //     }
+    //     $(selectElem).html($options.html());
+    // };
 
     // Append label and select tags after first element which is Templates
     var setFieldsToDom = function (parentElem, schema){
@@ -147,42 +146,42 @@ module.exports = function (config) {
     };
 
     // Fix hidden fields. This is necessary to make second request to backend module
-    var setHiddenToForm = function (data) {
-        for (var key in data) {
-            $('#containerStage2 form').append('<input type="hidden" value="'
-                + data[key] + '" name="'+ key +'"/>')
-        }
-    };
+    // var setHiddenToForm = function (data) {
+    //     for (var key in data) {
+    //         $('#containerStage2 form').append('<input type="hidden" value="'
+    //             + data[key] + '" name="'+ key +'"/>')
+    //     }
+    // };
 
-    // Handling response from backend module
-    $('#uploadFrame').load(function () {
-        var response = JSON.parse($('#uploadFrame').contents().find('body pre').html());
+    // // Handling response from backend module
+    // $('#uploadFrame').load(function () {
+    //     var response = JSON.parse($('#uploadFrame').contents().find('body pre').html());
 
-        // check if server throwed an error
-        // TODO: set this announcements to DOM
-        if (response['error']) {
-            console.log(response['error']);
-        } else if (response['success']) {
-            console.log(response['success']);
-        }
-        if (response['data']) {
-            // set array element to each select
-            data = response['data'];
-            loadTemplates();
-            setHiddenToForm({
-                file: response['file'],
-                header: response['header']
-            });
-        }
-    });
+    //     // check if server throwed an error
+    //     // TODO: set this announcements to DOM
+    //     if (response['error']) {
+    //         console.log(response['error']);
+    //     } else if (response['success']) {
+    //         console.log(response['success']);
+    //     }
+    //     if (response['data']) {
+    //         // set array element to each select
+    //         data = response['data'];
+    //         loadTemplates();
+    //         setHiddenToForm({
+    //             file: response['file'],
+    //             header: response['header']
+    //         });
+    //     }
+    // });
 
     // Rerender files by specified template
-    $('#containerStage2 #template').change(function () {
-        $('#containerStage2 form .control-group:not(:first)').remove();
+    // $('#containerStage2 #template').change(function () {
+    //     $('#containerStage2 form .control-group:not(:first)').remove();
 
-        var selected = $(this).find('option:selected').val();
-        setTemplateFields(templates, selected);
-    });
+    //     var selected = $(this).find('option:selected').val();
+    //     setTemplateFields(templates, selected);
+    // });
 
 }
 
