8a4178affc52de8d8192c870f41b83ef551b33a9
Luca Boieru <luca.boieru@yahoo.com>
2013-10-29 18:39:16 +0200

    Added fields to export lists.

--- ui_export.js
+++ ui_export.js
@@ -8,14 +8,20 @@ module.exports = function () {
     self.config.export.ui.selectors.hideUi = self.config.export.ui.selectors.hideUi || ".close";
     self.config.export.ui.selectors.target = self.config.export.ui.selectors.target || "#export";
     self.config.export.ui.selectors.exportList = self.config.export.ui.selectors.exportList || "#export-items";
-    self.config.export.ui.selectors.otherList = self.config.export.ui.selectors.exportList || "#other-items";
+    self.config.export.ui.selectors.otherList = self.config.export.ui.selectors.otherList || "#other-items";
     self.config.export.ui.selectors.listsClass = self.config.export.ui.selectors.listsClass || ".export-lists";
+    self.config.export.ui.selectors.listItem = self.config.export.ui.selectors.listItem || ".item-template";
+    self.config.export.ui.selectors.filename = self.config.export.ui.selectors.filename || "#filename";
+    self.config.export.ui.selectors.headers = self.config.export.ui.selectors.headers || "#headers";
+    self.config.export.ui.selectors.email = self.config.export.ui.selectors.email || "#email";
+    self.config.export.ui.selectors.separator = self.config.export.ui.selectors.separator || "#separator";
     
     // configure internal events
     
     // configure external events
     self.on('reset', reset);
     self.on('showUi', showUi);
+    self.on('setExportFields', setFields);
     
     // configure UI
     initUi.call(self);
@@ -62,7 +68,53 @@ function showUi () {
 }
 
 function reset () {
-    // TODO export reset stuff
+    var self = this;
+    
+    $(self.config.export.ui.selectors.filename).val("");
+    $(self.config.export.ui.selectors.headers).prop('checked', false);
+    $(self.config.export.ui.selectors.email).prop('checked', false);
+    $(self.config.export.ui.selectors.separator).val("COMMA");
+    
+    // TODO empty the lists
+}
+    
+function setFields (template) {
+    var self = this;
+    var tableFields = [];
+    var otherFields = [];
+    
+    // reseting UI
+    
+    reset.call(self);
+    
+    // TODO handle undefined variable
+    
+    for(var field in template.schema) {
+        // Skip keys that begin with "_"
+        if (!template.schema.hasOwnProperty(field)) continue;
+        if (field.toString().charAt(0) === "_") continue;
+        if ("hidden" in template.schema[field] && template.schema[field].hidden) {
+            if (typeof template.schema[field].label === "object")
+                otherFields.push(template.schema[field].label[M.getLocale()]);
+        } else {
+            if (typeof template.schema[field].label === "object")
+                tableFields.push(template.schema[field].label[M.getLocale()]);
+        }
+    }
+    
+    // update export UI
+    for(var i = 0, l = tableFields.length; i < l; ++ i) {
+        var myField = $(self.config.export.ui.selectors.exportList + ">li:first").clone();
+        myField.find(self.config.export.ui.selectors.listItem).html(tableFields[i]);
+        $(self.config.export.ui.selectors.exportList).append(myField);
+    }
+    for(var i = 0, l = otherFields.length; i < l; ++ i) {
+        var myField = $(self.config.export.ui.selectors.otherList + ">li:first").clone();
+        myField.find(self.config.export.ui.selectors.listItem).html(otherFields[i]);
+        $(self.config.export.ui.selectors.otherList).append(myField);
+    } 
+    $(self.config.export.ui.selectors.exportList + ">li:first-child").hide();
+    $(self.config.export.ui.selectors.otherList + ">li:first-child").hide();
 }
 
 return module; });
